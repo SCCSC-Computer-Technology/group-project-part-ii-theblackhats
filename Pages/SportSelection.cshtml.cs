952a565bd138d75069927ca5563cc2aaf139e841
AlexCarter43 <156260672+AlexCarter43@users.noreply.github.com>
2024-03-24 22:32:51 -0400

    Add files via upload

---
+++ Pages/SportSelection.cshtml.cs
@@ -0,0 +1,12 @@
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace TeamBlackHats.Pages
+{
+    public class SportSelectionModel : PageModel
+    {
+        public void OnGet()
+        {
+        }
+    }
+}
