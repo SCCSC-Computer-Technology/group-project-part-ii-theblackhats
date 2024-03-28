79ef751e6a391b7b26f6e3b5ac8e77a772f626a3
WillChamness <wzchamness@gmail.com>
2024-03-28 17:01:40 -0400

    feat: create new API for MLB
    allow GET for all MLB stats

---
+++ TeamBlackHatsAPI/Controllers/MLBController.cs
@@ -0,0 +1,25 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using System.Data;
+
+namespace TeamBlackHatsAPI.Controllers
+{
+    [Route("api/mlb")]
+    [ApiController]
+    public class MLBController : ControllerBase
+    {
+    public class UsersController : ControllerBase
+    {
+        [HttpGet]
+        [Route("stats")]
+        public JsonResult GetAllMLBPlayerStats()
+        {
+            string query = "select * from dbo.MLBStats";
+            DataTable table = NFLDBConnection.ReadWithSql(query);
+
+            return new JsonResult(table);
+
+        }
+    }
+    }
+}
